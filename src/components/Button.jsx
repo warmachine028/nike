87c3318e40fb955eba0ffa8590098ff013b8130a
AdityaAB4 <adityabarde4ab@gmail.com>
2023-08-25 19:58:33 +0530

    typo corrected

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,7 +9,7 @@ const Button = ({
   return (
     <>
       <button
-        className={`flex buttonjustify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full hover:bg-white hover:text-coral-red hover:border-coral-red ${
+        className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full hover:bg-white hover:text-coral-red hover:border-coral-red ${
           backgroundColor
             ? `${backgroundColor} ${borderColor} ${textColor}`
             : "bg-coral-red  border-coral-red text-white"
