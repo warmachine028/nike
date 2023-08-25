03bfe0b1907317780bc86a7877d2702dcfd47e07
AdityaAB4 <adityabarde4ab@gmail.com>
2023-08-25 19:59:43 +0530

    typo corrected

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,26 +7,24 @@ const Button = ({
   icon,
 }) => {
   return (
-    <>
-      <button
-        className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full hover:bg-white hover:text-coral-red hover:border-coral-red ${
-          backgroundColor
-            ? `${backgroundColor} ${borderColor} ${textColor}`
-            : "bg-coral-red  border-coral-red text-white"
-        }
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full hover:bg-white hover:text-coral-red hover:border-coral-red ${
+        backgroundColor
+          ? `${backgroundColor} ${borderColor} ${textColor}`
+          : "bg-coral-red  border-coral-red text-white"
+      }
 			${fullWidth && "w-full"}
 		 `}
-      >
-        {children}
-        {icon && (
-          <img
-            src={icon}
-            alt="arrow-right"
-            className="ml-2 rounded-full w-5 h-5"
-          />
-        )}
-      </button>
-    </>
+    >
+      {children}
+      {icon && (
+        <img
+          src={icon}
+          alt="arrow-right"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
+    </button>
   );
 };
 
