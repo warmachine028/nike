434feb7d10063fe16ba558d0947ec41cd99608bf
AdityaAB4 <adityabarde4ab@gmail.com>
2023-08-25 15:15:29 +0530

    hover effect added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,51 @@
-const Button = ({ children, icon, backgroundColor, textColor, borderColor, fullWidth }) => {
-	return (
-		<button
-			className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full ${
-				backgroundColor ? `${backgroundColor} ${borderColor} ${textColor}` : "bg-coral-red  border-coral-red text-white"
-			}
-			${fullWidth && 'w-full'}
-		 `}
-		>
-			{children}
-			{icon && <img src={icon} alt="arrow-right" className="ml-2 rounded-full w-5 h-5" />}
-		</button>
-	)
-}
+const Button = ({
+  children,
+  backgroundColor,
+  borderColor,
+  textColor,
+  fullWidth,
+  icon,
+}) => {
+  const styles = `
+    .button {
+      background-color: "coral-red";
+      border-color: "coral-red";
+      color: "white";
+    }
 
-export default Button
+    .button:hover {
+      background-color: #ffff;
+      border-color: #FF6452;
+      color: #FF6452;
+
+    }
+  `;
+  console.log("borderColor", borderColor);
+  return (
+    <>
+      <button
+        className={`flex button ${
+          styles.className
+        } justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full ${
+          backgroundColor
+            ? `${backgroundColor} ${borderColor} ${textColor}`
+            : "bg-coral-red  border-coral-red text-white"
+        }
+			${fullWidth && "w-full"}
+		 `}
+      >
+        {children}
+        {icon && (
+          <img
+            src={icon}
+            alt="arrow-right"
+            className="ml-2 rounded-full w-5 h-5"
+          />
+        )}
+      </button>
+      <style>{styles}</style>
+    </>
+  );
+};
+
+export default Button;
