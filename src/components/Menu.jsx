510805276baa4b7192d7728474fccb57c3d7c952
franco di silvestro <fdisilvestro@dc.uba.ar>
2023-09-09 19:31:56 -0300

    continued working on the menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
-import { useMenuContext } from '../contexts/MenuContext'
+import { useMenuContext, useMenuUpdateContext } from '../contexts/MenuContext'
+import { navLinks } from '../constants'
 
 const Menu = () => {
   const showMenu = useMenuContext()
+  const toggleShowMenu = useMenuUpdateContext()
 
-  return <div className={'z-50 fixed top-0 right-0 ' + (showMenu ? 'lg:hidden' : 'hidden')}>Menu</div>
+  return (
+    <div className={'z-50 absolute right-0 w-10/12 bg-coral-red h-screen ' + (showMenu ? 'lg:hidden' : 'hidden')}>
+      <ul>
+        <li className="flex justify-end text-slate-gray mt-2 mr-3 font-bold text-2xl">
+          <span onClick={toggleShowMenu}>X</span>
+        </li>
+        {navLinks.map((item) => (
+          <li key={item.label}>
+            <a
+              href={item.href}
+              className="flex justify-between mx-4 my-6 font-montserrat leading-normal text-2xl text-slate-gray hoverUnderline dark:mix-blend-difference"
+            >
+              <span>{item.label}</span>
+              <span>{' >'}</span>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default Menu
