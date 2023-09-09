5b473bd7bdcbd8be9b0a6e1eb0a65f5f198e3c1b
franco di silvestro <fdisilvestro@dc.uba.ar>
2023-09-09 13:22:56 -0300

    Color and pointer

--- src/components/ThemeSwitch.jsx
+++ src/components/ThemeSwitch.jsx
@@ -1,20 +1,20 @@
 /** @format */
-"use client"
-import { useTheme } from "next-themes"
-import { SunIcon } from "@heroicons/react/24/outline"
+'use client'
+import { useTheme } from 'next-themes'
+import { SunIcon } from '@heroicons/react/24/outline'
 
 const ThemeSwitch = () => {
-	const { systemTheme, theme, setTheme } = useTheme()
-	const currentTheme = theme === "system" ? systemTheme : theme
+  const { systemTheme, theme, setTheme } = useTheme()
+  const currentTheme = theme === 'system' ? systemTheme : theme
 
-	return (
-		<div className="inline-flex items-center">
-			<SunIcon
-				className={`mr-3 h-6 w-6 transition duration-300 ${currentTheme === "dark" ? "text-gray-100" : "text-slate-gray"}`}
-				onClick={() => (theme == "dark" ? setTheme("light") : setTheme("dark"))}
-			/>
-		</div>
-	)
+  return (
+    <div className="inline-flex items-center">
+      <SunIcon
+        className={`mr-3 h-6 w-6 transition duration-300 cursor-pointer ${currentTheme === 'dark' ? 'text-coral-red' : 'text-slate-gray'}`}
+        onClick={() => (theme == 'dark' ? setTheme('light') : setTheme('dark'))}
+      />
+    </div>
+  )
 }
 
 export default ThemeSwitch
