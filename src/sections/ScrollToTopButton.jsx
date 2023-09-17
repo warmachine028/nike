318fdcb41a702e50d6d262b34b054778173cafd8
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2023-09-17 23:20:15 +0530

    Create ScrollToTopButton.jsx

---
+++ src/sections/ScrollToTopButton.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import chevronUpSvg from '../assets/icons/chaveron-up.svg'; 
+
+const ScrollToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      const sectionHeight = window.innerHeight;
+      const scrollThreshold = sectionHeight * 2;
+
+      setIsVisible(scrollY > scrollThreshold);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <button
+      className={`fixed right-4 bottom-4 bg-transparent text-blue-500 py-2 px-4 rounded-full ${
+        isVisible ? 'block' : 'hidden'
+      }`}
+      onClick={scrollToTop}
+    >
+      <img
+        src={chevronUpSvg}
+        alt="Chevron Up"
+        className={`transform rotate-180 ${isVisible ? 'block' : 'hidden'}`}
+        style={{ background: 'none' }}
+      />
+    </button>
+  );
+};
+
+export default ScrollToTopButton;
