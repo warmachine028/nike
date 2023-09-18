d0b8780db37fe26c2e24454e338a208204a24b1b
SkyDe <sky_de1991@outlook.com>
2023-09-19 01:19:28 +0330

    migrate to new branch newSide

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,54 +1,39 @@
-import React from 'react'
-import { useMenuContext, useMenuUpdateContext, useMenuCloseContext } from '../contexts/MenuContext'
-import { navLinks } from '../constants'
+import { useMenuContext, useMenuUpdateContext } from "../contexts/MenuContext";
+import { navLinks } from "../constants";
 
 const Menu = () => {
-  const showMenu = useMenuContext()
-  const toggleShowMenu = useMenuUpdateContext()
-  // const dontShowMenu = useMenuCloseContext()
-  // const [windowWidth, setWindowWidth] = React.useState(window.innerWidth)
-
-  // const updateWidth = () => {
-  //   setWindowWidth(window.innerWidth)
-  // }
-
-  // React.useEffect(() => {
-  //   window.addEventListener('resize', updateWidth)
-  //   return () => {
-  //     window.removeEventListener('resize', updateWidth)
-  //   }
-  // }, [windowWidth])
-
-  // React.useEffect(() => {
-  //   if (windowWidth > 1024) {
-  //     dontShowMenu
-  //   }
-  // }, [windowWidth])
+  const showMenu = useMenuContext();
+  const toggleShowMenu = useMenuUpdateContext();
 
   return (
-    <>
-      {showMenu ? 'True' : 'False'}
-      <div className={'z-50 absolute right-0 w-10/12 bg-coral-red h-screen ' + (showMenu ? 'lg:hidden' : 'hidden')}>
-        <ul>
-          <li className="flex justify-end text-slate-gray mt-2 mr-3 font-bold text-2xl">
-            <span onClick={toggleShowMenu}>X</span>
+    <aside
+      className={`z-50 absolute right-0 w-10/12 flex flex-col items-center justify-evenly bg-coral-red dark:bg-white bg- h-screen border opacity-[0.95] ${
+        showMenu ? "lg:hidden" : "hidden"
+      }`}
+    >
+      <ul className="mt-0 w-full">
+        <li className="flex justify-end text-slate-gray mt-2 mr-3 font-bold text-2xl"></li>
+        {navLinks.map((item) => (
+          <li key={item.label} className="px-2">
+            <a
+              href={item.href}
+              className="flex justify-between mx-4 my-6 font-montserrat leading-normal text-2xl text-slate-gray hoverUnderline dark:mix-blend-difference"
+              onClick={toggleShowMenu}
+            >
+              <span className="text-white">{item.label}</span>
+              <span className="text-white">{" >"}</span>
+            </a>
           </li>
-          {navLinks.map((item) => (
-            <li key={item.label}>
-              <a
-                href={item.href}
-                className="flex justify-between mx-4 my-6 font-montserrat leading-normal text-2xl text-slate-gray hoverUnderline dark:mix-blend-difference"
-                onClick={toggleShowMenu}
-              >
-                <span>{item.label}</span>
-                <span>{' >'}</span>
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
-  )
-}
+        ))}
+      </ul>
+      <button
+        className="border p-4 px-8 text-white hover:opacity-80"
+        onClick={toggleShowMenu}
+      >
+        Close
+      </button>
+    </aside>
+  );
+};
 
-export default Menu
+export default Menu;
