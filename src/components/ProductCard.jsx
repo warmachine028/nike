986add3fd8106e3482b43a7f3b501d0bffe90414
Shrikrushna Patil <shri.medisage@gmail.com>
2023-10-02 20:14:00 +0530

    [FEATURE 💡] Product Card to be Change

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,17 +1,32 @@
-import { star } from "../assets/icons"
+import { star } from "../assets/icons";
 
 const ProductCard = ({ imgURL, name, price }) => {
-	return (
-		<div className="flex flex-1 flex-col w-full max-sm:w-full hover:scale-105 duration-300 ease-in-out">
-			<img src={imgURL} alt={`${name}-product`} width={282} height={282} />
-			<div className="mt-8 flex justify-start gap-2.5">
-				<img src={star} alt="rating" height={24} width={24} />
-				<p className="font-montserrat text-xl leading-normal text-slate-gray">4.5</p>
-			</div>
-			<h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">{name}</h3>
-			<p className="mt-2 font-semibold font-montserrat text-coral-red text-2xl leading-normal">{price}</p>
-		</div>
-	)
-}
+  return (
+    <div className="flex flex-col w-full max-sm:w-full hover:scale-105 duration-300 ease-in-out border rounded-lg p-4 shadow-lg">
+      <img
+        src={imgURL}
+        alt={`${name}-product`}
+        width={282}
+        height={282}
+        className="rounded-lg"
+      />
 
-export default ProductCard
+      <div className="mt-4 flex items-center">
+        <img src={star} alt="rating" height={24} width={24} className="mr-2" />
+        <p className="font-montserrat text-lg leading-normal text-slate-gray">
+          4.5
+        </p>
+      </div>
+
+      <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">
+        {name}
+      </h3>
+
+      <p className="mt-2 text-xl leading-normal font-semibold font-montserrat text-coral-red">
+        {price}
+      </p>
+    </div>
+  );
+};
+
+export default ProductCard;
