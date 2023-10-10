0d53ea610d988ae18ac393eacb59e01af689e307
KriteshTimsina <kriteshtimsina55@gmail.com>
2023-10-10 19:08:21 +0545

    fix:dark mode toggle not working for first time

--- src/components/ThemeSwitch.jsx
+++ src/components/ThemeSwitch.jsx
@@ -7,20 +7,17 @@ import { SunIcon, ShoppingCartIcon, MoonIcon } from '@heroicons/react/24/outline
 const ThemeSwitch = () => {
   const navigate = useNavigate();
   const { theme, setTheme } = useTheme();
-
-  const toggleTheme = () => (theme === 'dark' ? setTheme('light') : setTheme('dark'));
-
   return (
     <div className="inline-flex items-center">
-      {theme === 'dark' ? (
-        <SunIcon
-          className="mr-3 h-6 w-6 transition duration-300 cursor-pointer text-slate-gray dark:text-coral-red"
-          onClick={toggleTheme}
-        />
-      ) : (
+      {theme === 'light' ? (
         <MoonIcon
           className="mr-3 h-6 w-6 transition duration-300 cursor-pointer text-slate-gray dark:text-coral-red"
-          onClick={toggleTheme}
+          onClick={() => setTheme('dark')}
+        />
+      ) : (
+        <SunIcon
+          className="mr-3 h-6 w-6 transition duration-300 cursor-pointer text-slate-gray dark:text-coral-red"
+          onClick={() => setTheme('light')}
         />
       )}
       <ShoppingCartIcon
