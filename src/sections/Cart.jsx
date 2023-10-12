13f7d06dd868763be722ea0354842e8653266209
Irfan Salim <irfansalim77@gmail.com>
2023-10-12 18:02:19 +0000

    Add condition to check for items in cart and if not show a message

--- src/sections/Cart.jsx
+++ src/sections/Cart.jsx
@@ -36,7 +36,7 @@ const Cart = () => {
   };
 
   useEffect(() => {
-    const cartData = JSON.parse(localStorage.getItem('cart'));
+    const cartData = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : {};
     updateCartTotals(cartData);
     setCartItems(cartData);
   }, []);
@@ -53,85 +53,93 @@ const Cart = () => {
           </div>
           <div className="mx-auto mt-8 max-w-xl md:mt-12 rounded-3xl bg-white dark:text-gray-400 dark:bg-gray-900 overflow-hidden shadow-lg">
             <div className="px-4 py-6 sm:px-8 sm:py-10">
-              <ul className="-my-8">
-                {Object.keys(cartItems).map((item) => (
-                  <li
-                    key={item}
-                    className="flex flex-col space-y-3 py-6 text-left sm:flex-row sm:space-x-5 sm:space-y-0"
-                  >
-                    <div className="shrink-0 relative">
-                      <span className="absolute top-1 left-1 flex h-6 w-6 items-center justify-center rounded-full bg-white dark:bg-gray-700 dark:text-gray-300 text-sm font-medium text-gray-500 shadow sm:-top-2 sm:-right-2">
-                        {cartItems[item].quantity}
-                      </span>
-                      <img
-                        className="h-24 w-24 max-w-full rounded-lg object-cover"
-                        src={cartItems[item].imgURL}
-                        alt=""
-                      />
-                    </div>
-                    <div className="relative flex flex-1 flex-col justify-between">
-                      <div className="sm:col-gap-5 sm:grid sm:grid-cols-2">
-                        <div className="pr-8 sm:pr-5">
-                          <p className="text-base font-semibold text-gray-900 dark:text-gray-300">
-                            {cartItems[item].name}
-                          </p>
-                          <p className="mx-0 mt-1 mb-0 text-sm text-gray-400">36EU - 4US</p>
-                        </div>
+              {cartItems && Object.keys(cartItems).length > 0 ? (
+                // Render the cart content when there are items
+                <ul className="-my-8">
+                  {Object.keys(cartItems).map((item) => (
+                    <li
+                      key={item}
+                      className="flex flex-col space-y-3 py-6 text-left sm:flex-row sm:space-x-5 sm:space-y-0"
+                    >
+                      <div className="shrink-0 relative">
+                        <span className="absolute top-1 left-1 flex h-6 w-6 items-center justify-center rounded-full bg-white dark:bg-gray-700 dark:text-gray-300 text-sm font-medium text-gray-500 shadow sm:-top-2 sm:-right-2">
+                          {cartItems[item].quantity}
+                        </span>
+                        <img
+                          className="h-24 w-24 max-w-full rounded-lg object-cover"
+                          src={cartItems[item].imgURL}
+                          alt=""
+                        />
+                      </div>
+                      <div className="relative flex flex-1 flex-col justify-between">
+                        <div className="sm:col-gap-5 sm:grid sm:grid-cols-2">
+                          <div className="pr-8 sm:pr-5">
+                            <p className="text-base font-semibold text-gray-900 dark:text-gray-300">
+                              {cartItems[item].name}
+                            </p>
+                            <p className="mx-0 mt-1 mb-0 text-sm text-gray-400">36EU - 4US</p>
+                          </div>
 
-                        <div className="mt-4 flex items-end justify-between sm:mt-0 sm:items-start sm:justify-end">
-                          <p className="shrink-0 w-20 text-base font-semibold text-gray-900 dark:text-gray-300 sm:text-right">
-                            ${cartItems[item].price.toFixed(2)}
-                          </p>
+                          <div className="mt-4 flex items-end justify-between sm:mt-0 sm:items-start sm:justify-end">
+                            <p className="shrink-0 w-20 text-base font-semibold text-gray-900 dark:text-gray-300 sm:text-right">
+                              ${cartItems[item].price.toFixed(2)}
+                            </p>
+                          </div>
+                        </div>
+                        <div className="absolute top-0 right-0 flex sm:bottom-5 sm:top-auto">
+                          <button
+                            onClick={() => reduceQuantity(item)}
+                            type="button"
+                            className="flex rounded p-2 text-center text-gray-500 transition-all duration-200 ease-in-out focus:shadow hover:text-gray-900 dark:hover:text-gray-300"
+                          >
+                            <svg
+                              className="h-5 w-5"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M6 12H18"
+                                className=""
+                              ></path>
+                            </svg>
+                          </button>
+                          <button
+                            onClick={() => removeItem(item)}
+                            type="button"
+                            className="flex rounded p-2 text-center text-gray-500 transition-all duration-200 ease-in-out focus:shadow hover:text-gray-900 dark:hover:text-gray-300"
+                          >
+                            <svg
+                              className="h-5 w-5"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M6 18L18 6M6 6l12 12"
+                                className=""
+                              ></path>
+                            </svg>
+                          </button>
                         </div>
                       </div>
-                      <div className="absolute top-0 right-0 flex sm:bottom-5 sm:top-auto">
-                        <button
-                          onClick={() => reduceQuantity(item)}
-                          type="button"
-                          className="flex rounded p-2 text-center text-gray-500 transition-all duration-200 ease-in-out focus:shadow hover:text-gray-900 dark:hover:text-gray-300"
-                        >
-                          <svg
-                            className="h-5 w-5"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth="2"
-                              d="M6 12H18"
-                              className=""
-                            ></path>
-                          </svg>
-                        </button>
-                        <button
-                          onClick={() => removeItem(item)}
-                          type="button"
-                          className="flex rounded p-2 text-center text-gray-500 transition-all duration-200 ease-in-out focus:shadow hover:text-gray-900 dark:hover:text-gray-300"
-                        >
-                          <svg
-                            className="h-5 w-5"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth="2"
-                              d="M6 18L18 6M6 6l12 12"
-                              className=""
-                            ></path>
-                          </svg>
-                        </button>
-                      </div>
-                    </div>
-                  </li>
-                ))}
-              </ul>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                // Render a message when the cart is empty
+                <div className="text-center py-8">
+                  <p className="text-gray-500 dark:text-gray-400">Your cart is empty. Add some items to your cart!</p>
+                </div>
+              )}
               <div className="mt-6 space-y-3 border-t border-b py-8">
                 <div className="flex items-center justify-between">
                   <p className=" text-gray-800 dark:text-gray-400">Subtotal</p>
