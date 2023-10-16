e3da1de58c73dd4c6dd6b91c814e11e172ffd24b
Hanzalah Waheed <hanzalah.w@gmail.com>
2023-10-17 01:27:18 +0400

    add transitions to CustomerReviews

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -7,19 +7,18 @@ import { useEffect, useState } from 'react';
 const CustomerReviews = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [autoplay, setAutoplay] = useState(true);
+
   const updateIndex = (newIndex) => {
     if (newIndex < 0) {
-      newIndex = 0;
-    } else if (newIndex >= reviews.length) {
       newIndex = reviews.length - 1;
+    } else if (newIndex >= reviews.length) {
+      newIndex = newIndex % reviews.length;
     }
-
     setActiveIndex(newIndex);
   };
 
   useEffect(() => {
     let intervalFunc;
-
     if (autoplay) {
       intervalFunc = setInterval(() => {
         updateIndex((activeIndex + 1) % reviews.length);
@@ -35,7 +34,7 @@ const CustomerReviews = () => {
   };
 
   return (
-    <section className="bg-pale-blue padding dark:bg-slate-700">
+    <section className="bg-pale-blue padding dark:bg-slate-700 flex justify-center items-center flex-col">
       <div className="max-container">
         <h3 className="font-palanquin text-4xl font-bold text-center dark:text-gray-100">
           What Our
@@ -47,49 +46,61 @@ const CustomerReviews = () => {
         </p>
       </div>
 
-      <div className="mt-20 justify-center items-center right-0 flex">
-        <ReviewCard key={reviews[activeIndex].customerName} {...reviews[activeIndex]} />
-      </div>
-
-      <div className="carousel-buttons">
-        <button
-          className="button-arrow"
-          onClick={() => {
-            updateIndex(activeIndex - 1);
+      <div className="carousel">
+        <div
+          className="inner"
+          style={{
+            transform: `translate(-${activeIndex * 100}%)`
           }}
         >
-          <span className="material-symbols-outlined">arrow_back_ios</span>{' '}
-        </button>
-        <div className="indicators">
-          {reviews.map((item, index) => {
-            return (
-              <button
-                className="indicator-buttons"
-                key={index}
-                onClick={() => {
-                  updateIndex(index);
-                }}
-              >
-                <span
-                  className={`material-symbols-outlined ${
-                    index === activeIndex ? 'indicator-symbol-active' : 'indicator-symbol'
-                  }`}
-                >
-                  radio_button_checked
-                </span>
-              </button>
-            );
+          {reviews.map((review, index) => {
+            return <ReviewCard review={review} key={index} />;
           })}
         </div>
-        <button
-          className="button-arrow"
-          onClick={() => {
-            updateIndex(activeIndex + 1);
-          }}
-        >
-          <span className="material-symbols-outlined">arrow_forward_ios</span>
-        </button>
+
+        <div className="carousel-buttons">
+          <button
+            className="button-arrow"
+            onClick={() => {
+              updateIndex(activeIndex - 1);
+            }}
+          >
+            <span className="material-symbols-outlined">arrow_back_ios</span>{' '}
+          </button>
+
+          <div className="indicators">
+            {reviews.map((review, index) => {
+              return (
+                <button
+                  className="indicator-buttons"
+                  key={index}
+                  onClick={() => {
+                    updateIndex(index);
+                  }}
+                >
+                  <span
+                    className={`material-symbols-outlined ${
+                      index === activeIndex ? 'indicator-symbol-active' : 'indicator-symbol'
+                    }`}
+                  >
+                    radio_button_checked
+                  </span>
+                </button>
+              );
+            })}
+          </div>
+
+          <button
+            className="button-arrow"
+            onClick={() => {
+              updateIndex(activeIndex + 1);
+            }}
+          >
+            <span className="material-symbols-outlined">arrow_forward_ios</span>
+          </button>
+        </div>
       </div>
+
       <div className="justify-center flex">
         <button onClick={handlePlayPauseToggle}>
           <span className="material-symbols-outlined ">{autoplay ? 'pause' : 'play_arrow'}</span>
