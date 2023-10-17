78cff28f855b3439e731cee99d0d1c4f3a281d3f
Hanzalah Waheed <hanzalah.w@gmail.com>
2023-10-17 10:58:08 +0400

    add transition to testimonials

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,15 +1,17 @@
 import { star } from '../assets/icons';
 
-const ReviewCard = ({ imgURL, feedback, rating, customerName }) => {
+const ReviewCard = ({ review }) => {
   return (
-    <div className="inline-flex justify-center items-center flex-col">
-      <img src={imgURL} alt="customer" className="rounded-full object-cover" width={120} height={120} />
-      <article className="mt-6 max-w-sm text-center info-text dark:text-gray-300">{feedback}</article>
+    <div className="inline-flex justify-center items-center flex-col mt-6" style={{ width: '100%' }}>
+      <img src={review.imgURL} alt="customer" className="rounded-full object-cover" width={120} height={120} />
+      <article className="carousel-item-text mt-6 w-full text-center dark:text-gray-300 info-text">
+        {review.feedback}
+      </article>
       <div className="mt-3 flex justify-center items-center gap-2.5">
         <img src={star} alt="star" width={24} height={24} className="object-contain m-0" />
-        <p className="text-xl font-montserrat text-slate-gray dark:text-gray-300">{rating}</p>
+        <p className="text-xl font-montserrat text-slate-gray dark:text-gray-300">{review.rating}</p>
       </div>
-      <h3 className="mt-1 font-palanquin text-center text-3xl font-black dark:text-gray-200">{customerName}</h3>
+      <h3 className="mt-1 font-palanquin text-center text-3xl font-black dark:text-gray-200">{review.customerName}</h3>
     </div>
   );
 };
